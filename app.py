27fced85479eed1ed55cd3f74b1b645bc825c10c
nachi9211 <nachi9211@gmail.com>
2019-06-20 19:58:51 +0530

    Simpler website
    Title
    Link for each section
    The report file is currently unused, probably will remove later

--- app.py
+++ app.py
@@ -11,22 +11,29 @@ def game():
 
 @app.route("/")
 def main():
+    return render_template('index.html')
+
+@app.route('/kripke')
+def show_models():
     manager = Game(5,4,5)
     print (manager.playerlist)
     manager.deal_initial()
     manager.print_player_info()
     manager.play_game()
-    return render_template('index.html',
+    return render_template('kripke.html',
                            manager=manager)
 
-@app.route('/kripke')
-def show_models():
-    return render_template('kripke.html')
-
-
-@app.route('/report')
+@app.route('/introduction')
 def show_report():
-    return render_template('report.html')
+    return render_template('introduction.html')
+
+@app.route('/rules')
+def show_rules():
+    return render_template('rules.html')
+
+@app.route('/strategy')
+def show_strategy():
+    return render_template('strategy.html')
 
 if __name__ == '__main__':
     app.run()
