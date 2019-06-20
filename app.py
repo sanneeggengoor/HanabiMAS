143eb11b95d0b1410591cbb16fa8c4c7ec8cce73
nachi9211 <nachi9211@gmail.com>
2019-06-20 07:30:56 +0530

    Variables in html

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, flash, redirect, render_template, request, session, abo
 import os
 import json
 import pandas as pd
+from gameloop import *
 
 app = Flask(__name__)
 
@@ -10,7 +11,13 @@ def game():
 
 @app.route("/")
 def main():
-    return render_template('index.html')
+    manager = Game(5,4,5)
+    print (manager.playerlist)
+    manager.deal_initial()
+    manager.print_player_info()
+    manager.play_game()
+    return render_template('index.html',
+                           manager=manager)
 
 @app.route('/kripke')
 def showTable():
